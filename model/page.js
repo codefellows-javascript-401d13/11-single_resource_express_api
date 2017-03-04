b7d891e81b0dd5a0511036c1477683e81b1e2e99
darms <darmcca@gmail.com>
2017-03-04 13:13:23 -0800

    my route is being really really stupid

--- model/page.js
+++ model/page.js
@@ -2,14 +2,14 @@
 
 const uuid = require('node-uuid');
 const createError = require('http-errors');
-const debug = require('debug')('11-single_resource_express_api:page');
+const debug = require('debug')('page');
 const storage = require('../lib/storage.js');
 
 const Page = module.exports = function(title, steak) {
   debug('page constructor');
 
   if (!title) throw createError(400, 'expected title');
-  if (!meat) throw createError(400, 'expected steak');
+  if (!steak) throw createError(400, 'expected steak');
 
   this.id = uuid.v1();
   this.title = title;
@@ -29,5 +29,5 @@ Page.createPage = function(_page) {
 
 Page.fetchPage = function(id) {
   debug('fetch page');
-  return storage.fetchItem('note', id);
+  return storage.fetchItem('page', id);
 };
--- server.js
+++ server.js
@@ -1,18 +1,26 @@
 'use strict';
 
 const express = require('express');
-const Promise = require('bluebird');
+// const Promise = require('bluebird');
 const morgan = require('morgan');
 const createError = require('http-errors');
 const jsonParser = require('body-parser').json();
-const debug = require('debug')('note:server');
+const debug = require('debug')('page:server');
 
 const Page = require('./model/page.js');
 const storage = require('./lib/storage.js');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(morgan('dev'));
 
-app.
+app.get('/test', function(req, res) {
+  debug('GET: /test');
+  res.json({ msg: 'test route worked'});
+});
+
+app.listen(PORT, () => {
+  debug(`server up: ${PORT}`);
+  console.log('whatt up!');
+});
