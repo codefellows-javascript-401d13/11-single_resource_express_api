d7bba0c2a892211020049f78e79c4793ce69b5c7
darms <darmcca@gmail.com>
2017-03-05 12:43:14 -0800

    completed server

--- server.js
+++ server.js
@@ -14,13 +14,43 @@ const app = express();
 
 app.use(morgan('dev'));
 
-app.get('/test', function(req, res) {
-  debug('GET: /test');
-  res.json({ msg: 'test route worked'});
+app.post('/api/page', jsonParser, function(req, res, next) {
+  debug('POST: /api/page');
+  Page.createPage(req.body)
+  .then(page => res.json(page))
+  .catch(err =>> next(err));
+});
+
+app.get('/api/page/:id', function(req, res, next) {
+  debug('GET: /api/page');
+});
+
+app.fetchPage(req.params.id)
+.then(page => res.json(page))
+.catch(err => next(err));
+});
+
+app.delete('/api/page:id', function(req, res, next) {
+  debug('DELETE: /api/page');
+
+  Page.deletePage(req.params.id)
+  .then( () => res.sendSTatus(204))
+  .catch( err => next(err));
+});
+
+app.use(function(err, req, res, next) {
+  debug('error middleware');
+
+  if (err.status) {
+    res.status(err.status).send(err.name);
+    return;
+  }
+
+  err = createError(500, err.message);
+  res.status(err.status).send(err.name);
 });
 
 
 app.listen(PORT, () => {
   debug(`server up: ${PORT}`);
-  console.log('whatt up!');
 });
